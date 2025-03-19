d87e24e948ee5c11d468c1f093319a9945e6afba
Pradeep-1409 <pradeepaele14@gmail.com>
2025-03-20 00:07:02 +0530

    Update application.dart

--- application/bin/application.dart
+++ application/bin/application.dart
@@ -1,11 +1,11 @@
 import 'package:application/application.dart' as application;
-//import 'dart:io';
+import 'dart:io';
 void main(List<String> arguments) {
  application.switch1();
- //print("enter a month");
- //String month = stdin.readLineSync()!;
- //String? deep=application.deep(month);
- //print(deep);
+ print("enter a month");
+ String month = stdin.readLineSync()!;
+ String? deep=application.deep(month);
+ print(deep);
 
  
 
